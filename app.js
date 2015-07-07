629f4020d494ae31fd9d797c77510c1234ecda32
SPARTAN157 <kkelekolio21@punahou.edu>
2015-07-06 14:28:31 -1000

    Major Sync (“It Works!”)

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+	function fcookie(){
+var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "You will be rich", "You will fall", "You will be poor");
+var thing = [Math.floor(Math.random() * 4)];
+document.getElementById("cookie").innerHTML = random[thing];
+}
