6a4096373305a626e9783ee56ecb6f80af00c5e3
SPARTAN157 <kkelekolio21@punahou.edu>
2015-07-08 13:02:53 -1000

    bruh

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-$('#for').hover(function(){
-	image.style.width = '22%';
-	image.style.height = 'auto';
-
-});
-	
-
-	function fcookie(){
-var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "You will be rich", "You will fall", "You will be poor");
-var thing = [Math.floor(Math.random() * 4)];
-document.getElementById("cookie").innerHTML = random[thing];
+function fcookie()
+{
+	var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "You will be rich", "You will fall", "You will be poor");
+	var thing = [Math.floor(Math.random() * 4)];
+	document.getElementById("cookie").innerHTML = random[thing];
 }
