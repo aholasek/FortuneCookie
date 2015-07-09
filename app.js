ce8c3ada018c5146ada2437b919f6004649978a8
SPARTAN157 <kkelekolio21@punahou.edu>
2015-07-08 14:01:48 -1000

    dfouds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function fcookie()
 {
-	var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "1", "2", "3");
+	var English = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "1", "2", "3");
+	var Spanish = new Array("Trate mas tarde", "Uno", "Dos", "Tres");
 	var thing = [Math.floor(Math.random() * 4)];
 	document.getElementById("cookie").innerHTML = random[thing];
 }
