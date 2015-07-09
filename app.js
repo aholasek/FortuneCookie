b7c78d66ed61059e1edbafc85e5ed330d6b948f6
SPARTAN157 <kkelekolio21@punahou.edu>
2015-07-09 13:03:33 -1000

    d

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function fcookie()
 {
+	
 	var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "1", "2", "3");
 	var thing = [Math.floor(Math.random() * 4)];
 	document.getElementById("cookie").innerHTML = random[thing];
