8155bedf78e083bea658c31af452419d87bc6a26
SPARTAN157 <kkelekolio21@punahou.edu>
2015-07-08 14:24:41 -1000

    dsfg

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 function fcookie()
 {
-	var English = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "1", "2", "3");
-	var Spanish = new Array("Trate mas tarde", "Uno", "Dos", "Tres");
-	var  a = list[English], b = list(Spanish);
-	list[Spanish] = a;
-	list[English] = b;
+	var random = new Array("Try Again Later... I'm Sleeping... [Yes, Fortune Cookies Need Their Sleep Too]", "1", "2", "3");
 	var thing = [Math.floor(Math.random() * 4)];
-	document.getElementById("cookie").innerHTML = [thing];
+	document.getElementById("cookie").innerHTML = random[thing];
 }
